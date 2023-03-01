4d1dddcda25df153b58bd2da3229fe500ab40450
Chuck Jungmann <chuck@cpjj.net>
2023-03-01 13:53:47 -0600

    To prevent optimization of using source string in place of copy, I made and use a random string generator for another test.

--- minimal.c
+++ minimal.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 const char* SArray[] = {
    "First String",
@@ -17,13 +18,45 @@ const char* SArray[] = {
 
 int Len_SArray = sizeof(SArray) / sizeof(char*);
 
+
+char get_numbered_char(long rnum)
+{
+   rnum %= 62;
+   if (rnum < 26)
+      return 65 + rnum;        // upper-case A-Z
+   else if (rnum < 52)
+      return 97 + rnum - 26;   // lower-case a-z
+   else
+      return 48 + rnum - 52;   // 0-9
+}
+
+void test_numbered_char(int value)
+{
+   printf("Character returned for %d is %c.\n", value, get_numbered_char(value));
+}
+
+void get_random_line(const char **line, int *len)
+{
+   // Random length between 5 and 25 characters:
+   int loclen = (rand() % 20) + 5;
+   // Buffer big enough to contain the longest generated string
+   static char buffer[26];
+
+   char *ptr = buffer;
+   char *end = ptr + loclen;
+
+   for ( ; ptr < end; ++ptr)
+      *ptr = get_numbered_char(rand());
+   *ptr = '\0';
+
+   *line = buffer;
+   *len = loclen;
+}
+
 void copy_array(void)
 {
-   int len = 4;
+   int len = 8;
    char **array = (char **)malloc(len * sizeof(char*));
-
-   printf("Locally-malloced target array, "
-          "local code to malloc-copy-assign source strings.\n");
 
    char **tptr = array;
    char **tend = tptr + len;
@@ -31,7 +64,7 @@ void copy_array(void)
    const char **sptr = SArray;
    const char **send = sptr + Len_SArray;
 
-   while (tptr < tend && sptr < send)
+   for (; tptr < tend && sptr < send; ++tptr, ++sptr)
    {
       int slen = strlen(*sptr);
       char *buff = (char*)malloc(slen+1);
@@ -41,14 +74,34 @@ void copy_array(void)
          buff[slen] = '\0';
          *tptr = buff;
       }
+   }
+}
 
-      ++tptr;
-      ++sptr;
+void fill_array(void)
+{
+   int len = 12;
+   char **array = (char**)malloc(len * sizeof(char*));
+   char **ptr = array;
+   char **end = ptr + len;
+
+   const char *line;
+   int line_len;
+
+   for (; ptr < end; ++ptr)
+   {
+      get_random_line(&line, &line_len);
+      char *buff = (char*)malloc(line_len+1);
+      memcpy(buff, line, line_len);
+      buff[line_len] = '\0';
+      *ptr = buff;
    }
 }
 
 int main(int argc, const char **argv)
 {
-   copy_array();
+   // srand(time(NULL));
+   // copy_array();
+   fill_array();
+
    return 0;
 }
