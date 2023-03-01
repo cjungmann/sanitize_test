e1b4bdf35228ecaf79ae0c1dfa903f86ef8bf846
Chuck Jungmann <chuck@cpjj.net>
2023-03-01 14:40:17 -0600

    Moved and renamed get_random_line() from test2.c to minimal.c, along with supporting functions.

--- minimal.c
+++ minimal.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
 const char* SArray[] = {
    "First String",
@@ -17,41 +16,6 @@ const char* SArray[] = {
 };
 
 int Len_SArray = sizeof(SArray) / sizeof(char*);
-
-
-char get_numbered_char(long rnum)
-{
-   rnum %= 62;
-   if (rnum < 26)
-      return 65 + rnum;        // upper-case A-Z
-   else if (rnum < 52)
-      return 97 + rnum - 26;   // lower-case a-z
-   else
-      return 48 + rnum - 52;   // 0-9
-}
-
-void test_numbered_char(int value)
-{
-   printf("Character returned for %d is %c.\n", value, get_numbered_char(value));
-}
-
-void get_random_line(const char **line, int *len)
-{
-   // Random length between 5 and 25 characters:
-   int loclen = (rand() % 20) + 5;
-   // Buffer big enough to contain the longest generated string
-   static char buffer[26];
-
-   char *ptr = buffer;
-   char *end = ptr + loclen;
-
-   for ( ; ptr < end; ++ptr)
-      *ptr = get_numbered_char(rand());
-   *ptr = '\0';
-
-   *line = buffer;
-   *len = loclen;
-}
 
 void copy_array(void)
 {
@@ -77,31 +41,8 @@ void copy_array(void)
    }
 }
 
-void fill_array(void)
-{
-   int len = 12;
-   char **array = (char**)malloc(len * sizeof(char*));
-   char **ptr = array;
-   char **end = ptr + len;
-
-   const char *line;
-   int line_len;
-
-   for (; ptr < end; ++ptr)
-   {
-      get_random_line(&line, &line_len);
-      char *buff = (char*)malloc(line_len+1);
-      memcpy(buff, line, line_len);
-      buff[line_len] = '\0';
-      *ptr = buff;
-   }
-}
-
 int main(int argc, const char **argv)
 {
-   // srand(time(NULL));
-   // copy_array();
-   fill_array();
-
+   copy_array();
    return 0;
 }
--- test2.c
+++ test2.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 const char* SArray[] = {
    "First String",
@@ -17,12 +18,26 @@ const char* SArray[] = {
 
 int Len_SArray = sizeof(SArray) / sizeof(char*);
 
+/**
+ * @brief Set elements of an array of char* to NULL.
+ *
+ * @param "array"   array whose elements should be NULLed
+ * @param "len"     number of elements to be NULLed
+ */
 void null_init_strings_array(const char **array, int len)
 {
    int asize = len * sizeof(char*);
    memset(array, 0, asize);
 }
 
+/**
+ * @brief Frees and sets to NULL each member of an array.
+ *
+ * Be careful not to use the function on a uninitialized array.
+ *
+ * @param "array"    array of pointers to char* whose elements must be freed
+ * @param "len"      number of elements to free
+ */
 void free_array(char **array, int len)
 {
    char **ptr = array;
@@ -33,6 +48,76 @@ void free_array(char **array, int len)
       *ptr = NULL;
 
       ++ptr;
+   }
+}
+
+/**
+ * @brief Given an integer value, returns a character from the set A-Za-z0-9.
+ *
+ * @param "rnum"   integer value to convert to a char
+ *
+ * @return Associated char value from A-Za-z0-9
+ */
+char get_numbered_char(long rnum)
+{
+   rnum %= 62;
+   if (rnum < 26)
+      return 65 + rnum;        // upper-case A-Z
+   else if (rnum < 52)
+      return 97 + rnum - 26;   // lower-case a-z
+   else
+      return 48 + rnum - 52;   // 0-9
+}
+
+/**
+ * @brief Generates a random string of random length (5-25 characters)
+ *
+ * This function is an unlimited source of random text strings that
+ * can be used in place of the array of strings defines at the top of
+ * this source file.
+ *
+ * @param[out] "line"    Pointer to char* in which the generated text
+ *                       will be indicated.
+ * @param[out] "len"     Number of characters in the generated text.
+ */
+void get_random_text_string(const char **line, int *len)
+{
+   // Random length between 5 and 25 characters:
+   int loclen = (rand() % 20) + 5;
+   // Buffer big enough to contain the longest generated string
+   static char buffer[26];
+
+   char *ptr = buffer;
+   char *end = ptr + loclen;
+
+   for ( ; ptr < end; ++ptr)
+      *ptr = get_numbered_char(rand());
+   *ptr = '\0';
+
+   *line = buffer;
+   *len = loclen;
+}
+
+/**
+ * @brief Fill an array with randomly-generated strings.
+ */
+void fill_array(void)
+{
+   int len = 12;
+   char **array = (char**)malloc(len * sizeof(char*));
+   char **ptr = array;
+   char **end = ptr + len;
+
+   const char *line;
+   int line_len;
+
+   for (; ptr < end; ++ptr)
+   {
+      get_random_text_string(&line, &line_len);
+      char *buff = (char*)malloc(line_len+1);
+      memcpy(buff, line, line_len);
+      buff[line_len] = '\0';
+      *ptr = buff;
    }
 }
 
